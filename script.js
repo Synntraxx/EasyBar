3faf4f09e72ef81495103c3d579114eba73a208f
Matthis Sanier <saniermatthis@gmail.com>
2026-08-14 18:43:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -264,7 +264,7 @@ document.addEventListener('DOMContentLoaded', () => {
         if (!dateStr) return '';
         const parts = dateStr.split('-');
         if (parts.length === 3) {
-            return `${parts[2]}/${parts[1]}`;
+            return `${parts[2]}/${parts[1]}/${parts[0]}`;
         }
         return dateStr;
     }
